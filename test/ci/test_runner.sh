80e99d5436b69c05bbe334a402cc2baa5cda5352
Tim Krajcar <allegro@conmolto.org>
2015-11-15 16:35:07 -0600

    Prime first, then run multiverse

--- test/ci/test_runner.sh
+++ test/ci/test_runner.sh
@@ -4,5 +4,6 @@ set -e
 if [ "$TYPE" = "UNIT" ]; then
   bundle exec rake test:env[$BRANCH]
 elif [ "$TYPE" = "FUNCTIONAL" ]; then
+  bundle exec rake test:multiverse:prime[group=$GROUP] &&
   bundle exec rake test:multiverse[group=$GROUP]
 fi
