45e93c11c125a01ce36e61969160e5a8495799fb
Tim Krajcar <allegro@conmolto.org>
2015-11-18 11:00:36 -0800

    Remove old CI scripts, use ENVIRONMENT instead of BRANCH for unit test variants

--- test/ci/test_runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$TYPE" = "UNIT" ]; then
-  bundle exec rake test:env[$BRANCH]
-elif [ "$TYPE" = "FUNCTIONAL" ]; then
-  bundle exec rake test:multiverse:prime[group=$GROUP] &&
-  bundle exec rake test:multiverse[group=$GROUP]
-fi
