ee0fa8ab5b045fd8d2742f26c2c7f776ed18cada
Tim Krajcar <allegro@conmolto.org>
2015-11-19 15:37:43 -0800

    CI: Force verbose mode

--- test/script/ci.sh
+++ test/script/ci.sh
@@ -4,6 +4,6 @@ set -e
 if [ "$TYPE" = "UNIT" ]; then
   bundle exec rake test:env[$ENVIRONMENT]
 elif [ "$TYPE" = "FUNCTIONAL" ]; then
-  bundle exec rake test:multiverse:prime[group=$GROUP] &&
-  bundle exec rake test:multiverse[group=$GROUP]
+  bundle exec rake test:multiverse:prime[group=$GROUP,verbose] &&
+  bundle exec rake test:multiverse[group=$GROUP,verbose]
 fi
