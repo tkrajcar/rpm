76a02853c2b8945d5359721fb0ecf22b6d9e70e6
Tim Krajcar <allegro@conmolto.org>
2015-11-19 16:00:35 -0800

    CI: force nocache on multiverse bundle

--- test/script/ci.sh
+++ test/script/ci.sh
@@ -5,5 +5,5 @@ if [ "$TYPE" = "UNIT" ]; then
   bundle exec rake test:env[$ENVIRONMENT]
 elif [ "$TYPE" = "FUNCTIONAL" ]; then
 #  bundle exec rake test:multiverse:prime[group=$GROUP,verbose] &&
-  bundle exec rake test:multiverse[group=$GROUP,verbose]
+  bundle exec rake test:multiverse[group=$GROUP,verbose,nocache]
 fi
