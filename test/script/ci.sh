f824985c961cfe1a42d8ff3d5f48997604b7508d
Tim Krajcar <allegro@conmolto.org>
2015-11-24 15:13:44 -0800

    CI: More ENV foolery

--- test/script/ci.sh
+++ test/script/ci.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "NEW_RELIC_ENV set to: $NEW_RELIC_ENV"
+unset MERB_ENV
+unset RUBY_ENV
+unset RACK_ENV
+unset RAILS_ENV
+
 echo "RUBY_ENV set to: $RUBY_ENV"
 echo "RACK_ENV set to: $RACK_ENV"
 echo "RAILS_ENV set to: $RAILS_ENV"
